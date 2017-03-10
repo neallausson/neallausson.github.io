8802bfd86c7c53090eefab395dbd82c41fa608e1
neallausson <neallausson@gmail.com>
2017-03-10 14:03:44 +0100

    Add files via upload

--- bitcoin.js
+++ bitcoin.js
@@ -31,7 +31,26 @@ $(document).ready(function() {
 		},
 
 		error : function(xhr, status, err) {
-			$('#bitcoin_block_number').append(err+" N/A");
+			$('#blockchaininfo.blocks').append(err+" N/A");
+		}
+	});
+});
+
+$(document).ready(function() {
+	$.ajax({
+		url : "http://bitcoin.mubiz.com/info",
+		dataType : "json",
+		contentType : "application/json; charset=utf-8",
+		type : "GET",
+		timeout:	"5000",
+		async : false,
+
+		success : function(data) {
+			$('#info.connections').append(data.connections);
+		},
+
+		error : function(xhr, status, err) {
+			$('#info.connections').append(err+" N/A");
 		}
 	});
 });
