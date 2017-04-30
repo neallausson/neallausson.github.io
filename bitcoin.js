a5102f2bb1fc5af3ebc764a6c79c6d5a19f49152
neallausson <neallausson@gmail.com>
2017-04-30 19:56:04 +0200

    Add files via upload

--- bitcoin.js
+++ bitcoin.js
@@ -1,24 +1,4 @@
 
-
-$(document).ready(function() {
-	$.ajax({
-		url : "https://bitcoin.mubiz.com/blockchaininfo",
-		dataType : "json",
-		contentType : "application/json; charset=utf-8",
-		type : "GET",
-		timeout:	"5000",
-		async : false,
-
-		success : function(data) {
-			$('#blockchaininfo_blocks').append(data.blocks);
-		},
-
-		error : function(xhr, status, err) {
-			$('#blockchaininfo_blocks').append(err+" N/A");
-		}
-	});
-});
-
 $(document).ready(function() {
 	$.ajax({
 		url : "https://bitcoin.mubiz.com/info",
@@ -29,35 +9,17 @@ $(document).ready(function() {
 		async : false,
 
 		success : function(data) {
+			$('#blockchaininfo_blocks').append(data.blocks);
 			$('#info_connections').append(data.connections),
 			$('#info_version').append(data.version),
 			$('#info_protocolversion').append(data.protocolversion);
 		},
 
 		error : function(xhr, status, err) {
+			$('#blockchaininfo_blocks').append(err+" N/A");
 			$('#info_connections').append(err+" N/A"),
 			$('#info_version').append(err+" N/A"),
 			$('#info_protocolversion').append(err+" N/A");
 		}
 	});
 });
-
-
-/*$(document).ready(function() {
-	$.ajax({
-		url : "http://bitcoin.mubiz.com/peerinfo",
-		dataType : "json",
-		contentType : "application/json; charset=utf-8",
-		type : "GET",
-		timeout:	"5000",
-		async : false,
-
-		success : function(data) {
-			$('#info_connections').append(data.connections);
-		},
-
-		error : function(xhr, status, err) {
-			$('#info_connections').append(err+" N/A");
-		}
-	});
-});*/
